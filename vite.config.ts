8907c3f092fe2093fe7b099898e9201f721b7655
web-studio-mami <web.studio.mami@gmail.com>
2025-11-12 10:24:03 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/UrbanBloom/tree/gh-pages',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
